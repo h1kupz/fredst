8d7655af82e009429f3600c10d1840ce8e6c9595
h1kupz <h1kupz3z@gmail.com>
2022-05-26 23:30:50 +0700

    Favicon Again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,12 +19,6 @@ export default defineNuxtConfig({
     htmlAttrs: {
       lang: "en",
     },
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
-    ],
     link: [
       {
         rel: "icon",
