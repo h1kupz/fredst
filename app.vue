26b454d117df2d0e30a2fc81da37965a7add2872
h1kupz <h1kupz3z@gmail.com>
2022-05-26 16:42:34 +0700

    Favicon

--- app.vue
+++ app.vue
@@ -3,34 +3,3 @@
     <NuxtPage />
   </div>
 </template>
-
-<script>
-export default {
-  head() {
-    return {
-      pwa: {
-        icon: {
-          filename: "favicon.png?v1",
-        },
-      },
-      title: "Fres St.",
-      meta: [
-        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
-        {
-          hid: "Fred St.",
-          name: "Fred St.",
-          content:
-            "Fred St offers a fresh and exciting landscape architectural design alternative for businesses operating in the Urban Development and Infrastructure sectors across Australia and who might be seeking a more personalized, creative and cost effective landscape architectural design service.",
-        },
-      ],
-      link: [
-        {
-          rel: "icon",
-          type: "image/png",
-          href: "../assets/favicon.png",
-        },
-      ],
-    };
-  },
-};
-</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,20 @@
-import { defineNuxtConfig } from 'nuxt'
+
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
+export default ({
+  head: {
+        title: 'Fred St.',
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          {
+            hid: 'Fred St.',
+            name: 'Fred St.',
+            content: 'Fred St offers a fresh and exciting landscape architectural design alternative for  businesses operating in the Urban Development and Infrastructure sectors across Australia and who might be seeking a more personalized, creative and cost effective landscape architectural design service.'
+          }
+        ],
+        link: [{ rel: 'icon', type: 'image/png', href: "/favicon.png" }]
+      },
     build: {
         postcss: {
           postcssOptions: {
@@ -20,17 +33,5 @@ export default defineNuxtConfig({
           filename: 'favicon.png?v1'
         }
       },
-      head: {
-        title: 'Fres St.',
-        meta: [
-          { charset: 'utf-8' },
-          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-          {
-            hid: 'description',
-            name: 'description',
-            content: 'Fred St offers a fresh and exciting landscape architectural design alternative for  businesses operating in the Urban Development and Infrastructure sectors across Australia and who might be seeking a more personalized, creative and cost effective landscape architectural design service.'
-          }
-        ],
-        link: [{ rel: 'icon', type: 'image/png', href: "/favicon.png" }]
-      },
+      
 })
