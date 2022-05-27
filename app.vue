8a7f3fc07cfdd0cca1bc34fbda2bc740f375f132
h1kupz <h1kupz3z@gmail.com>
2022-05-27 09:25:39 +0700

    Switch fav back to png

--- app.vue
+++ app.vue
@@ -12,8 +12,8 @@ export default {
       link: [
         {
           rel: "icon",
-          type: "image/x-icon",
-          href: `favicon.ico`,
+          type: "image/png",
+          href: `favicon.png`,
         },
       ],
     };
